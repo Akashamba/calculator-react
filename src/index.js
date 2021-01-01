9215b4eafe276b450f83634217d8e3e0aecf9637
Akashamba <akashambashankar@gmail.com>
2021-01-01 15:56:32 +0530

    fixed import error in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './service-worker.js'
+import * as serviceWorker from './serviceWorkerRegistration.js'
 
 ReactDOM.render(
   <React.StrictMode>
